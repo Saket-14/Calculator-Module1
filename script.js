f890f1cc79cde18eefbf52835055a7129df42a7a
Saket Singh <saket05.singh@gmail.com>
2023-11-12 19:46:14 +0530

    calculator

--- script.js
+++ script.js
@@ -2,46 +2,46 @@ let currentInput = "";
 let isResultShown = false;
 
 function appendToDisplay(value) {
-   if (isResultShown) {
-      document.getElementById("display").value = "";
-      isResultShown = false;
-   }
-   currentInput += value;
-   document.getElementById("display").value= currentInput;
+  if (isResultShown) {
+    document.getElementById("display").value = "";
+    isResultShown = false;
+  }
+  currentInput += value;
+  document.getElementById("display").value = currentInput;
 }
 
 function clearDisplay() {
-   currentInput = "";
-   document.getElementById("display").value = "";
+  currentInput = "";
+  document.getElementById("display").value = "";
 }
 
 function calcResult() {
-   try {
-      const result = eval(currentInput);
-      document.getElementById("display").value = result;
-      isResultShown = true;
-      currentInput = result.toString();
-   }  catch (error) {
-      document.getElementById("display").value = "Error";
-   }
+  try {
+    const result = eval(currentInput);
+    document.getElementById("display").value = result;
+    isResultShown = true;
+    currentInput = result.toString();
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
 
 function deleteLast() {
-   if (isResultShown) {
-      clearDisplay();
-   } else {
-      currentInput = currentInput.slice(0, -1);
-      document.getElementById("display").value =currentInput;
-   }
+  if (isResultShown) {
+    clearDisplay();
+  } else {
+    currentInput = currentInput.slice(0, -1);
+    document.getElementById("display").value = currentInput;
+  }
 }
 
 function calcPercentage() {
-   try {
-      const percentage = eval(currentInput) / 100;
-      document.getElementById("display").value= percentage;
-      isResultShown = true;
-      currentInput = percentage.toString();
-   }  catch (error) {
-      document.getElementById("display").value = "Error";
-   }
+  try {
+    const percentage = eval(currentInput) / 100;
+    document.getElementById("display").value = percentage;
+    isResultShown = true;
+    currentInput = percentage.toString();
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
